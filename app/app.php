2770b52fea2912f29c9fc15383a00aa28728ce7b
Elysia Nason <elysia.avery@gmail.com>
2017-01-25 11:06:41 -0800

    Add form validation

--- app/app.php
+++ app/app.php
@@ -16,6 +16,7 @@
     'twig.path' => __DIR__.'/../views'
   ));
 
+  use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\HttpFoundation\Request;
   Request::enableHttpMethodParameterOverride();
 
@@ -24,9 +25,29 @@
   });
 
   $app->post("/stylists", function() use ($app) {
-    $stylist = new Stylist($_POST['name']);
-    $stylist->save();
-    return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    if(isset($_POST['name'])) {
+      foreach($_POST as $val) {
+        if(trim($val) == '' || empty($val)) {
+          $error = 'You have entered an invalid name.';
+          return $app['twig']->render('error.html.twig', array('error' => $error));
+        } else {
+          $stylist = new Stylist($_POST['name']);
+          $stylist->save();
+          return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+        }
+      }
+    } else {}
+  });
+
+  $app->error(function (\Exception $e, Request $request, $code) {
+    switch ($code) {
+      case 404:
+        $message = 'The requested page could not be found.';
+        break;
+      default:
+        $message = 'We are sorry, but something went terribly wrong.';
+    }
+    return new Response($message);
   });
 
   $app->get("/stylists/{id}", function($id) use ($app) {
@@ -40,10 +61,18 @@
   });
 
   $app->patch("/stylists/{id}", function($id) use ($app) {
-    $name = $_POST['name'];
-    $stylist = Stylist::find($id);
-    $stylist->update($name);
-    return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    if(isset($_POST['name'])) {
+      foreach($_POST as $val) {
+        if(trim($val) == '' || empty($val)) {
+          $error = 'You have entered an invalid name.';
+          return $app['twig']->render('error.html.twig', array('error' => $error));
+        } else {}
+      }
+      $name = $_POST['name'];
+      $stylist = Stylist::find($id);
+      $stylist->update($name);
+      return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    } else {}
   });
 
   $app->delete("/stylists/{id}", function($id) use ($app) {
@@ -57,12 +86,20 @@
   });
 
   $app->post("/clients", function() use ($app) {
-    $name = $_POST['name'];
-    $stylist_id = $_POST['stylist_id'];
-    $client = new Client($name, $id = null, $stylist_id);
-    $client->save();
-    $stylist = Stylist::find($stylist_id);
-    return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    if(isset($_POST['name'])) {
+      foreach($_POST as $val) {
+        if(trim($val) == '' || empty($val)) {
+          $error = 'You have entered an invalid name.';
+          return $app['twig']->render('error.html.twig', array('error' => $error));
+        } else {}
+      }
+      $name = $_POST['name'];
+      $stylist_id = $_POST['stylist_id'];
+      $client = new Client($name, $id = null, $stylist_id);
+      $client->save();
+      $stylist = Stylist::find($stylist_id);
+      return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    } else {}
   });
 
   $app->get("/clients/{id}", function($id) use ($app) {
@@ -71,10 +108,18 @@
   });
 
   $app->patch("/clients/{id}", function($id) use ($app) {
-    $name = $_POST['name'];
-    $client = Client::find($id);
-    $client->update($name);
-    return $app['twig']->render('client.html.twig', array('client' => $client));
+    if(isset($_POST['name'])) {
+      foreach($_POST as $val) {
+        if(trim($val) == '' || empty($val)) {
+          $error = 'You have entered an invalid name.';
+          return $app['twig']->render('error.html.twig', array('error' => $error));
+        } else {}
+      }
+      $name = $_POST['name'];
+      $client = Client::find($id);
+      $client->update($name);
+      return $app['twig']->render('client.html.twig', array('client' => $client));
+    } else {}
   });
 
   $app->delete("/clients/{id}", function($id) use ($app) {
